1dcdf3f6a739faf908ddd394deaf8f2d887fb2c1
matheuslcn <matheuslcn@id.uff.br>
2026-07-19 21:43:52 -0300

    adicao do display info de task

--- src/Task.cpp
+++ src/Task.cpp
@@ -2,8 +2,14 @@
 #include "Stop.h"
 #include "Drone.h"
 #include "Package.h"
+#include <iostream>
+
+Task::Task(int id, int packageId, int dropId) :
+    id(id), packageId(packageId), dropId(dropId) {}
 
 void Task::displayInfo() const
 {
-    
+    std::cout << "Task: " << std::endl;
+    std::cout << "id:" << Task::id << ", package: " << Task::packageId << ", dropId: " << Task::dropId << std::endl;
+
 }
